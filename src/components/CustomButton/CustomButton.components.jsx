42db31e0aa0c569cd2c8518f2528a0d6aaf73f8d
Blackzero172 <supermeega@outlook.com>
2022-01-02 14:01:03 +0200

    Added Edit Card Styling and new Button Comp

--- src/components/CustomButton/CustomButton.components.jsx
+++ src/components/CustomButton/CustomButton.components.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./CustomButton.styles.css";
 const CustomButton = ({ children, style, styleClass }) => {
-	return (
-		<button type="button" className={styleClass} style={style}>
-			{children}
-		</button>
-	);
+  return (
+    <button type="button" className={styleClass} style={style}>
+      {children}
+    </button>
+  );
 };
 export default CustomButton;
--- src/components/EditCard/EditCard.components.jsx
+++ src/components/EditCard/EditCard.components.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import CustomButton from "../CustomButton/CustomButton.components";
 import "./EditCard.styles.css";
 const EditCard = ({ card }) => {
-	return (
-		<div className="edit-card">
-			{card.answer}
-			<CustomButton>Edit</CustomButton>
-		</div>
-	);
+  return (
+    <div className="edit-card">
+      <p className="card-name">Card Id: {card.id}</p>
+      <div className="card-answer">
+        <p>Answer:</p>
+        <p>{card.answer}</p>
+      </div>
+      <div className="button-contain">
+        <CustomButton>
+          <i className="fas fa-edit"></i> Edit
+        </CustomButton>
+        <CustomButton>
+          <i className="fas fa-trash"></i> Delete
+        </CustomButton>
+      </div>
+    </div>
+  );
 };
 export default EditCard;
